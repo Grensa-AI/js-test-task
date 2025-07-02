9a7ec3a6934605223a81ef80c3e6de889ef16588
TMaltseva <bacardeonie@gmail.com>
2025-07-02 15:15:58 -0700

    refactor: update App by using hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Title } from "./Components/Title/Title";
 import { Summary } from "./Components/Summary/Summary";
+import { useSummary } from "./hooks/useSummary";
 
 const AppContainer = styled.div`
   width: 400px;
@@ -19,10 +20,32 @@ const AppContainer = styled.div`
 `;
 
 export const App = () => {
+  const { summary, isLoading, error, isConfigured, generateSummary } =
+    useSummary();
+
+  useEffect(() => {
+    const handleMessage = (request) => {
+      if (request.action === "toggle") {
+        generateSummary();
+      }
+    };
+
+    if (window.chrome && chrome.runtime) {
+      chrome.runtime.onMessage.addListener(handleMessage);
+      return () => chrome.runtime.onMessage.removeListener(handleMessage);
+    }
+  }, [generateSummary]);
+
   return (
     <AppContainer>
       <Title />
-      <Summary />
+      <Summary
+        summary={summary}
+        isLoading={isLoading}
+        error={error}
+        onRefresh={generateSummary}
+        isConfigured={isConfigured}
+      />
     </AppContainer>
   );
 };
