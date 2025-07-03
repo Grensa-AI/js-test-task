ea7226271947b84ac17c92b9c4537abf56d706fd
mrKamanov <mr.kamanov@yandex.ru>
2025-07-04 02:26:38 +0300

    Добавляю конфигурацию craco для поддержки нескольких точек входа

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   webpack: {
@@ -25,6 +26,29 @@ module.exports = {
       };
 
       webpackConfig.optimization.runtimeChunk = false;
+
+      // Оставляем только main.js в index.html (options_page)
+      webpackConfig.plugins = webpackConfig.plugins.map(plugin => {
+        if (plugin.constructor.name === 'HtmlWebpackPlugin') {
+          plugin.options.chunks = ['main'];
+        }
+        return plugin;
+      });
+
+      // Фиксируем имя CSS-файла для content.js
+      const MiniCssExtractPlugin = webpackConfig.plugins.find(
+        p => p.constructor && p.constructor.name === 'MiniCssExtractPlugin'
+      );
+      if (MiniCssExtractPlugin) {
+        const origFn = MiniCssExtractPlugin.options.filename;
+        MiniCssExtractPlugin.options.filename = (pathData) => {
+          if (pathData.chunk && pathData.chunk.name === 'content') {
+            return 'static/css/content.css';
+          }
+          if (typeof origFn === 'function') return origFn(pathData);
+          return origFn || 'static/css/[name].[contenthash:8].css';
+        };
+      }
 
       return webpackConfig;
     },
