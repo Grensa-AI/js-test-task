9c7d01cdd4589c9fd2dc5897b63028d7d67fab4d
mrKamanov <mr.kamanov@yandex.ru>
2025-07-04 02:44:20 +0300

    Подключаю OptionsApp в index.js для отображения на странице настроек

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import OptionsApp from "./OptionsApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+if (window.location.href.includes('chrome-extension') || window.location.href.includes('extension://')) {
+  root.render(<OptionsApp />);
+} else {
+  root.render(<App />);
+}
