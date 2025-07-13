ca265f00ea21db5de79d733a649113e32c50f80d
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-07-13 09:48:39 +0200

    feat: connect content script messages with sw ai request logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Title } from "./Components/Title/Title";
 import { Summary } from "./Components/Summary/Summary";
+import useHashChange from "./hooks/useHashChange";
+import extractChat from "./utils/extractChat";
 
 const AppContainer = styled.div`
   width: 400px;
@@ -19,10 +21,41 @@ const AppContainer = styled.div`
 `;
 
 export const App = () => {
+  const [chatInfo, setChatInfo] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [summary, setSummary] = useState("");
+  const [error, setError] = useState("");
+  const hash = useHashChange();
+
+  useEffect(() => {
+    extractChat().then((chatInfo) => setChatInfo(chatInfo));
+  }, [hash])
+
+  useEffect(() => {
+    if (!chatInfo || Object.keys(chatInfo).length === 0) return;
+
+    setLoading(true);
+
+    chrome.runtime.sendMessage({ action: "get_summary", chatInfo }, (response) => {
+      setLoading(false)
+      if (chrome.runtime.lastError) {
+        setError(chrome.runtime.lastError.message);
+        return;
+      }
+      if (response?.error) {
+        setError(response.error);
+        setSummary(null);
+      } else {
+        setError(null);
+        setSummary(response.summary);
+      }
+    });
+    console.log(chatInfo);
+  }, [chatInfo])
   return (
     <AppContainer>
       <Title />
-      <Summary />
+      <Summary text={summary} loading={loading} error={error} />
     </AppContainer>
   );
 };
