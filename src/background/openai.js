443f94080efce016a328279ab74347b3795397ba
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-07-13 12:17:44 +0200

    feat: add summary caching

--- src/background/openai.js
+++ src/background/openai.js
@@ -1,7 +1,19 @@
-import { API, PROMPTS, MODEL } from "./contants";
+import cache from './cache';
 import { getApiKey } from "./storage";
+import { API, PROMPTS, MODEL } from "./contants";
+
 export async function getSummary(chatContent, sendResponse) {
   try {
+    // Check if summary already cached locally
+    const cached = await cache.get(chatContent.chatId);
+    if (cached && cached.summary) {
+      return sendResponse({
+        summary: cached.summary,
+        lastUpdated: cached.lastUpdated,
+        fromCache: true
+      });
+    }
+    // Not cached -> fetching new summary
     const api_key = await getApiKey();
     const res = await fetch(`${API.OPENAI_BASE_URL}/chat/completions`, {
       method: "POST",
@@ -19,10 +31,18 @@ export async function getSummary(chatContent, sendResponse) {
     const data = await res.json();
     if (data.error) {
       sendResponse({ error: data.error.message });
-    } else {
-      sendResponse({ summary: data.choices[0].message.content.trim() });
     }
+    const summary = data.choices[0].message.content.trim();
+
+    await cache.set(chatContent.chatId, summary, chatContent.messages.lenght);
+
+    return sendResponse({
+      summary,
+      lastUpdated: Date.now(),
+      fromCache: false
+    })
   } catch (err) {
+
     return sendResponse({ error: err.message });
   }
 }
